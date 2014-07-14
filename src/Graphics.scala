3609c251ed55aa87b0984bb870eee7f4f09a0ef2
Isaac Banner <isaacbanner@isaabann-mp1.enernoc.local>
2014-07-14 14:16:00 -0400

    done with graphics

--- src/Graphics.scala
+++ src/Graphics.scala
@@ -2,8 +2,8 @@
  * Created by isaacbanner on 7/11/14.
  */
 
-import scala.swing._
-import scala.swing.event.ButtonClicked
+import swing._
+import event._
 
 object Graphics extends SimpleSwingApplication{
   def top = new MainFrame {
@@ -14,15 +14,39 @@ object Graphics extends SimpleSwingApplication{
     val lbl1 = new Label{
       text = "This is a Label taking about Labels"
     }
+    object celsius extends TextField { columns = 5 }
+    object fahrenheit extends TextField { columns = 5 }
+
+    val fp = new FlowPanel {
+      contents += celsius
+      contents += new Label("Celsius  = ")
+      contents += fahrenheit
+      contents += new Label("Fahrenheit")
+      border = Swing.EmptyBorder(10, 10, 10, 10)
+    }
+
     contents = new BoxPanel(Orientation.Vertical) {
       contents += btn1
       contents += lbl1
+      contents += fp
       border = Swing.EmptyBorder(30, 30, 30, 30)
     }
+
     listenTo(btn1)
+    listenTo(celsius, fahrenheit)
     reactions += {
       case ButtonClicked(b) => {
         lbl1.text = "This is a Label talking about \nhow you pressed a Button!"
+      }
+      case EditDone(`fahrenheit`) => {
+        val f = fahrenheit.text.toInt
+        val c = (f - 32) * 5 / 9
+        celsius.text = c.toString
+      }
+      case EditDone(`celsius`) => {
+        val c = celsius.text.toInt
+        val f = c * 9 / 5 + 32
+        fahrenheit.text = f.toString
       }
     }
   }
